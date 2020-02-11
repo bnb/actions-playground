4637a227dac51f84ae817360d52ba4289b2f28d9
Tierney Cyren <accounts@bnb.im>
2020-02-11 08:45:46 -0800

    fix: code style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require('http')
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+  res.writeHead(200)
+  res.end('Hello, World!')
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(8080)
