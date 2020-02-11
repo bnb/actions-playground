968bec60922cb294c817e967022b1497d79a383e
Tierney Cyren <accounts@bnb.im>
2020-02-11 09:35:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const requestListener = function (req, res) {
   res.writeHead(200)
-  res.end('Hello, World!')
+  res.end('Hello, World')
 }
 
 const server = http.createServer(requestListener);
